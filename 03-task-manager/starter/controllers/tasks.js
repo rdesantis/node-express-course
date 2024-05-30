e08aa7a47eb676d45c13f06edc46d46a0617f8e6
Ronald DeSantis <Ronald.DeSantis.PMP@gmail.com>
2024-05-30 16:06:38 -0400

    Refactored custom error handler for future use

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,5 +1,6 @@
 const Task = require('../models/Task');
 const asyncWrapper = require('../middleware/async');
+const CustomAPIError = require('../errors/custom-error');
 
 const getAllTasks = asyncWrapper(async (req, res) => {
     const tasks = await Task.find({});
@@ -11,32 +12,32 @@ const createTask = asyncWrapper(async (req, res) => {
     res.status(201).json({task});
 });
 
-const getTask = asyncWrapper(async (req, res) => {
+const getTask = asyncWrapper(async (req, res, next) => {
     const {id} = req.params;
     const task = await Task.findById(id);
     if (!task) {
-        return res.status(404).json({msg: `No task with id: ${id}`})
+        return next(new CustomAPIError(`Task with id ${id} not found`, 404));
     }
     res.status(200).json({task});
 });
 
-const updateTask = asyncWrapper(async (req, res) => {
+const updateTask = asyncWrapper(async (req, res, next) => {
     const {id} = req.params;
     const task = await Task.findByIdAndUpdate(id, req.body, {
         new: true,
         runValidators: true
     });
     if (!task) {
-        return res.status(404).json({msg: `No task with id: ${id}`})
+        return next(new CustomAPIError(`Task with id ${id} not found`, 404));
     }
     res.status(200).json({task});
 });
 
-const deleteTask = asyncWrapper(async (req, res) => {
+const deleteTask = asyncWrapper(async (req, res, next) => {
     const {id} = req.params;
     const task = await Task.findByIdAndDelete(id);
     if (!task) {
-        return res.status(404).json({msg: `No task with id: ${id}`})
+        return next(new CustomAPIError(`Task with id ${id} not found`, 404));
     }
     res.status(200).json({task});
 });
